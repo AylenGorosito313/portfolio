04f25f01ab9bfc9a9475be1b883db26881f8f1fe
Aylen Gorosito <109639386+AylenGorosito313@users.noreply.github.com>
2022-12-27 10:36:28 -0300

    first committt

--- src/pages/proyects/Proyects.jsx
+++ src/pages/proyects/Proyects.jsx
@@ -1,29 +1,28 @@
 import React from "react";
 import "./Proyects.css";
-import imgRankA from "../../assets/Captura.png";
+import imgRankA from "../../assets/animerank.png";
 import pedia from "../../assets/pedia.png";
 export default function Proyects() {
   return (
     <div className="container-gneral">
       <div className="div-proyect">
         <div className="card-pro">
-            <div className="div-logo">
-
+          <div className="div-logo">
             <img className="logo" src={imgRankA} alt="app" />
-            </div>
-         
+          </div>
+
           <div className="div-titul-p">
             {" "}
             <a className="Link-proyecto" href="https://animerankig.vercel.app">
               {" "}
               <p className="title-proyect">AnimeRank</p>
+              <p className="p-proyect">Frondtend, API REST</p>
             </a>
-            <p className="p-proyect">Frondtend, API REST</p>
             <div>
               <p className="parrafo-proyecto ">
-                Primer proyecto en el cual consumo una api de animes para
-                obtener un listado del cual obtener un ranking.Tambien hago uso
-                de React para el front y manejo de Redux para estados globales{" "}
+                Proyecto en el cual consumo una api de animes para
+                obtener un listado del cual obtener un ranking y realizar busquedas . Tambien hago uso
+                de React para el front y Redux para estados globales{" "}
               </p>
             </div>
           </div>
@@ -31,21 +30,24 @@ export default function Proyects() {
       </div>
       <div className="div-proyect">
         <div className="card-pro">
-        <div className="div-logo-pedia">
-        <img className="logo-pedia" src={pedia} alt="app" />
-        </div>
-         
+          <div className="div-logo-pedia">
+            <img className="logo-pedia" src={pedia} alt="app" />
+          </div>
+
           <div className="div-titul-p">
             {" "}
-            <a className="Link-proyecto" href="https://pi-henry-front.vercel.app/">
+            <a
+              className="Link-proyecto"
+              href="https://pi-henry-front.vercel.app/"
+            >
               <p className="title-proyect">CountryPedia</p>
             </a>
             <p className="p-proyect">Frondtend , Backend y Bases de datos</p>
             <div>
               {" "}
               <p className="parrafo-proyecto ">
-                Primer proyecto en el cual implemento una base de datos, express
-                y sequelize en Backend. En utilizo React y Redux . Proyecto
+                En este proyecto  implemento una base de datos relacional SQL, Express
+                y Sequelize en Backend. En utilizo React y Redux . Proyecto
                 realizado como trabajo indiviudal de Henry{" "}
               </p>
             </div>
