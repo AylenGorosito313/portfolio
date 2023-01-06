8cbe6f19d89b90df08b51ba915e78cb681b65807
Aylen Gorosito <109639386+AylenGorosito313@users.noreply.github.com>
2023-01-06 06:11:58 -0300

    a

---
+++ src/pages/Contacto/Contact.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Mailer from "../../components/Contacto/Mailer";
+export default function Contact() {
+  return (
+    <>
+      <div>Contact</div>
+      <div>
+        <Mailer />
+      </div>
+    </>
+  );
+}
