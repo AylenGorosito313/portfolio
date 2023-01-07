eca7b34f63d6c13fc085d37e6e39e1391e17cb32
Aylen Gorosito <109639386+AylenGorosito313@users.noreply.github.com>
2023-01-07 18:31:55 -0300

    Update Cards.jsx

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -4,18 +4,17 @@ function Cards({ titulo, tecno, parrafo, img, link }) {
   return (
     <>
       <div className="div-proyect">
-     
         <div className="card-pro">
           <div className="div-logo-pedia">
             <img className="logo-pedia" src={img} alt="app" />
           </div>
+
           <div className="text-container">
             <div className="div-titul-p">
               {" "}
               <a className="Link-proyecto" href={link}>
                 <p className="title-proyect">{titulo}</p>
               </a>
-              
             </div>
             {/* <p className="p-proyect">{tecno}</p> */}
             <div className="style-text">
@@ -24,7 +23,6 @@ function Cards({ titulo, tecno, parrafo, img, link }) {
               </div>
             </div>
           </div>
-      
         </div>
       </div>
     </>
